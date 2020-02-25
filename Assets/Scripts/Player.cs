f2ed19a4289ec7437f03ea1c74da8d9ca4fdfe42
ManoTech272 <hermano272@gmail.com>
2020-02-25 11:55:54 +0000

    Before improvment

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -33,14 +33,14 @@ public class Player : MonoBehaviour
     private void ProcessTranslation()
     {
         horiz = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffset = horiz * xSpeed * Time.deltaTime;
+        float xOffset = horiz * xSpeed * Time.deltaTime;         // Offset = how much to move the ship every frame
 
         vert = CrossPlatformInputManager.GetAxis("Vertical");
         float yOffset = vert * xSpeed * Time.deltaTime;
 
         // x and y axis have switched due to correction in model direction
 
-        transform.localPosition = new Vector3(transform.localPosition.x, Mathf.Clamp(yOffset + transform.localPosition.y, -4.5f , 4.5f), Mathf.Clamp(xOffset + transform.localPosition.z, -11, 12));
+        transform.localPosition = new Vector3(transform.localPosition.x, Mathf.Clamp(yOffset + transform.localPosition.y, -4.5f , 4.5f), Mathf.Clamp(xOffset + transform.localPosition.z, -12, 12));
     }
 
     private void ProcessRotation()
